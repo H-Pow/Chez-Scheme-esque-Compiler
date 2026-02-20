702618d549b6f448d2ee8bc4580486213e9fe849
RainMa22 <d2rain@126.com>
2026-02-19 20:52:30 -0800

    purpose and signature

--- flatten-program.rkt
+++ flatten-program.rkt
@@ -3,8 +3,97 @@
 (require cpsc411/compiler-lib)
 
 (provide flatten-program)
-(define (flatten-program nal2)
-    (TODO nal2))
+
+;; block-asm-lang-v4
+; p	 	::=	 	(module b ... b)
+;   b	 	::=	 	(define label tail)
+;   tail	 	::=	 	(halt opand)
+;  	 	|	 	(jump trg)
+;  	 	|	 	(begin effect ... tail)
+;  	 	|	 	(if (relop loc opand) (jump trg) (jump trg))
+;   effect	 	::=	 	(set! loc triv)
+;  	 	|	 	(set! loc_1 (binop loc_1 opand))
+;   triv	 	::=	 	opand
+;  	 	|	 	label
+;   opand	 	::=	 	int64
+;  	 	|	 	loc
+;   trg	 	::=	 	label
+;  	 	|	 	loc
+;   loc	 	::=	 	reg
+;  	 	|	 	fvar
+;   reg	 	::=	 	rsp
+;  	 	|	 	rbp
+;  	 	|	 	rax
+;  	 	|	 	rbx
+;  	 	|	 	rcx
+;  	 	|	 	rdx
+;  	 	|	 	rsi
+;  	 	|	 	rdi
+;  	 	|	 	r8
+;  	 	|	 	r9
+;  	 	|	 	r12
+;  	 	|	 	r13
+;  	 	|	 	r14
+;  	 	|	 	r15
+;   binop	 	::=	 	*
+;  	 	|	 	+
+;   relop	 	::=	 	<
+;  	 	|	 	<=
+;  	 	|	 	=
+;  	 	|	 	>=
+;  	 	|	 	>
+;  	 	|	 	!=
+;   int64	 	::=	 	int64?
+;   fvar	 	::=	 	fvar?
+;   label	 	::=	 	label?
+;; --------------------------
+;; para-asm-lang-v4
+; p	 	::=	 	(begin s ...)
+;   s	 	::=	 	(halt opand)
+;  	 	|	 	(set! loc triv)
+;  	 	|	 	(set! loc_1 (binop loc_1 opand))
+;  	 	|	 	(jump trg)
+;  	 	|	 	(with-label label s)
+;  	 	|	 	(compare loc opand)
+;  	 	|	 	(jump-if relop trg)
+;   triv	 	::=	 	opand
+;  	 	|	 	label
+;   opand	 	::=	 	int64
+;  	 	|	 	loc
+;   trg	 	::=	 	label
+;  	 	|	 	loc
+;   loc	 	::=	 	reg
+;  	 	|	 	fvar
+;   reg	 	::=	 	rsp
+;  	 	|	 	rbp
+;  	 	|	 	rax
+;  	 	|	 	rbx
+;  	 	|	 	rcx
+;  	 	|	 	rdx
+;  	 	|	 	rsi
+;  	 	|	 	rdi
+;  	 	|	 	r8
+;  	 	|	 	r9
+;  	 	|	 	r12
+;  	 	|	 	r13
+;  	 	|	 	r14
+;  	 	|	 	r15
+;   binop	 	::=	 	*
+;  	 	|	 	+
+;   relop	 	::=	 	<
+;  	 	|	 	<=
+;  	 	|	 	=
+;  	 	|	 	>=
+;  	 	|	 	>
+;  	 	|	 	!=
+;   int64	 	::=	 	int64?
+;   fvar	 	::=	 	fvar?
+;   label	 	::=	 	label?
+
+;; block-asm-lang-v4 -> para-asm-lang-v4
+;; Compile Block-asm-lang v4 to Para-asm-lang v4 by flattening basic blocks into labeled instructions.
+(define (flatten-program bal4)
+  (TODO bal4))
 (module+ test
   (require rackunit)
   )

30e96bfae9198f5b1ec5fc097606793fc9fe94fd
RainMa22 <d2rain@126.com>
2026-02-20 16:44:42 -0800

    expose-basic-blocks: add test cases

--- expose-basic-blocks.rkt
+++ expose-basic-blocks.rkt
@@ -101,13 +101,23 @@
 (define (expose-basic-blocks p)
   (define blocks '())
   (define (create-block! label tail)
-    (append blocks `((define ,label ,tail)))
+    (set! blocks (cons `(define ,label ,tail) blocks))
     label)
   ;; nested-pred label label -> label
   ;; effect: creates a block with pred
   (define (expose-pred! pred truelab falselab)
-    (define label (fresh-label))
+    (define label (fresh-label 'pred))
     (match pred
+      [`(not ,pred) (expose-pred! pred falselab truelab)]
+      [`(begin ,fx* ... ,pred)
+       (foldr
+        expose-effect!
+        (expose-pred! pred truelab falselab)
+        fx* )]
+      [`(if ,pred ,pred1 ,pred2)
+       (expose-pred! pred
+                     (expose-pred! pred1 truelab falselab)
+                     (expose-pred! pred2 truelab falselab))]
       [`(,relop ,loc ,triv)
        (create-block! label `(if ,pred
                                  (jump ,truelab)
@@ -117,52 +127,84 @@
                                          (jump ,falselab)))]
       [`(false) (create-block! label `(if ,pred
                                           (jump ,truelab)
-                                          (jump ,falselab)))]
-      [`(not ,pred) (expose-pred! pred falselab truelab)]
-      [`(begin ,fx* ... ,pred)
-       (create-block! label
-                      `(jump ,(foldr
-                               expose-effect!
-                               (expose-pred! pred)
-                               fx* )))]
-      [`(if ,pred ,pred1 ,pred2)
-       (expose-pred! pred
-                     (expose-pred! pred1 truelab falselab)
-                     (expose-pred! pred2 truelab falselab))]))
+                                          (jump ,falselab)))]))
   ;; nested-effect label -> label
   ;; effect: creates a block with the effect
   (define (expose-effect! fx next)
-    (define label (fresh-label))
+    (define label (fresh-label 'fx))
     (match fx
-      [`(set! ,loc ,triv) (create-block!
-                           label
-                           `(begin ,fx
-                                   (jump ,next)))]
+      [`(set! ,loc ,triv)
+       (create-block!
+        label `(begin ,fx
+                      (jump ,next)))]
       [`(set! ,loc (,binop ,loc ,triv))
        (create-block! label
                       `(begin ,fx
                               (jump ,next)))]
+      [`(begin ,fx* ... ,fx)
+       (foldr expose-effect! (expose-effect! fx next) fx*)]
       [`(if ,pred ,fx1 ,fx2)
        (expose-pred! pred (expose-effect! fx1 next)
                      (expose-effect! fx2 next))]))
   ;; nested-tail -> label
   ;; effect: creates a block
   (define (expose-tail! tail)
-    (define label (fresh-label))
+    (define label (fresh-label 'tail))
     (match tail
       [`(halt ,_) (create-block! label tail)]
       [`(begin ,fx* ... ,tail)
-       (create-block! label
-                      `(jump ,(foldr expose-effect!
-                                     (expose-tail! tail)
-                                     fx*)))]
+       (foldr expose-effect!
+              (expose-tail! tail)
+              fx*)]
       [`(if ,pred ,tail1 ,tail2)
-       `(if ,(TODO pred) ,(expose-tail! tail1)
-            ,(expose-tail! tail2))]))
+       (expose-pred! pred (expose-tail! tail1)
+                     (expose-tail! tail2))]))
   (match p
     [`(module ,tail) (begin
                        (expose-tail! tail)
                        `(module ,@blocks))]))
 
 (module+ test
-  (require rackunit))
+  (require rackunit
+           cpsc411/langs/v4)
+
+  (define (peek x)
+    (pretty-display x)
+    x)
+  (define-syntax-rule (check-by-interp nal4)
+    (check-eq? (interp-nested-asm-lang-v4 nal4)
+               (interp-block-pred-lang-v4 (peek (expose-basic-blocks nal4)))))
+
+  (check-by-interp `(module (halt 1)))
+  (check-by-interp `(module (begin (halt 1))))
+  (check-by-interp `(module (begin (begin (halt 1)))))
+  (check-by-interp `(module (begin (set! rax 1)
+                                   (set! rdi 5)
+                                   (halt rdi))))
+  (check-by-interp `(module (begin (set! rax 1)
+                                   (set! rdi 5)
+                                   (if (> rax rdi)
+                                       (halt rdi)
+                                       (halt rax)))))
+  (check-by-interp `(module (begin (set! rax 1)
+                                   (begin
+                                     (set! rsp 5)
+                                     (set! rdi 5))
+                                   (if (> rax rdi)
+                                       (halt rdi)
+                                       (halt rax)))))
+  (check-by-interp `(module (begin (set! rax 1)
+                                   (if (begin
+                                         (set! rdi 5)
+                                         (> rax rdi))
+                                       (halt rdi)
+                                       (halt rax)))))
+  (check-by-interp `(module (begin (set! rax 1)
+                                   (set! rdi 5)
+                                   (set! rsp 9)
+                                   (if (if (< rsp rdi)
+                                           (> rax rdi)
+                                           (> rax rsp))
+                                       (halt rdi)
+                                       (halt rax)))))
+  )

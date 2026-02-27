13c2a9ec8fd703dc892a9101198405798bceabf5
tkozek <tkozek@students.cs.ubc.ca>
2026-02-27 12:47:20 -0800

    Edited and extended uniquify.

--- uniquify.rkt
+++ uniquify.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require cpsc411/compiler-lib)
+(require cpsc411/compiler-lib
+         cpsc411/langs/v5)
 
 (provide uniquify
          interp-values-lang)
@@ -14,19 +15,42 @@
     ['* x64-mul]))
 (define triv? (or/c name? int64?))
 
-; values-lang-v3
-; p	 	::=	 	(module tail)
-; tail	 	::=	 	value
-;  	|	 	(let ([x value] ...) tail)
-; value	 	::=	 	triv
-;  	|	 	(binop triv triv)
-;  	|	 	(let ([x value] ...) value)
-; triv	 	::=	 	int64
-;  	|	 	x
-; x	 	::=	 	name?
-; binop	 	::=	 	*
-;  	|	 	+
-; int64	 	::=	 	int64?
+; values-lang-v5
+;   p	 	::=	 	(module (define x (lambda (x ...) tail)) ... tail)
+
+;   pred	 	::=	 	(relop triv triv)
+;  	 	|	 	(true)
+;  	 	|	 	(false)
+;  	 	|	 	(not pred)
+;  	 	|	 	(let ([x value] ...) pred)
+;  	 	|	 	(if pred pred pred)
+
+;   tail	 	::=	 	value
+;  	 	|	 	(let ([x value] ...) tail)
+;  	 	|	 	(if pred tail tail)
+;  	 	|	 	(call x triv ...)
+
+;   value	 	::=	 	triv
+;  	 	|	 	(binop triv triv)
+;  	 	|	 	(let ([x value] ...) value)
+;  	 	|	 	(if pred value value)
+
+;   triv	 	::=	 	int64
+;  	 	|	 	x
+
+;   x	 	::=	 	name?
+
+;   binop	 	::=	 	*
+;  	 	|	 	+
+
+;   relop	 	::=	 	<
+;  	 	|	 	<=
+;  	 	|	 	=
+;  	 	|	 	>=
+;  	 	|	 	>
+;  	 	|	 	!=
+
+;   int64	 	::=	 	int64?
 
 (define (interp-values-lang vl3)
   (define (interp-triv triv env)
@@ -48,80 +72,144 @@
       [`(module ,tail) (interp-tail tail env)])))
 
 ;---------------------
-; values-unique-lang-v3
-; p	 	::=	 	(module tail)
+; values-unique-lang-v5
+;  p	 	::=	 	(module (define label (lambda (aloc ...) tail)) ... tail)
+;   pred	 	::=	 	(relop opand opand)
+;  	 	|	 	(true)
+;  	 	|	 	(false)
+;  	 	|	 	(not pred)
+;  	 	|	 	(let ([aloc value] ...) pred)
+;  	 	|	 	(if pred pred pred)
 ;   tail	 	::=	 	value
 ;  	 	|	 	(let ([aloc value] ...) tail)
+;  	 	|	 	(if pred tail tail)
+;  	 	|	 	(call triv opand ...)
 ;   value	 	::=	 	triv
-;  	 	|	 	(binop triv triv)
+;  	 	|	 	(binop opand opand)
 ;  	 	|	 	(let ([aloc value] ...) value)
-;   triv	 	::=	 	int64
+;  	 	|	 	(if pred value value)
+;   opand	 	::=	 	int64
 ;  	 	|	 	aloc
-;   binop	 	::=	 	*
-;  	 	|	 	+
-;   aloc	 	::=	  aloc?
+;   triv	 	::=	 	opand
+;  	 	|	 	label
+;   binop	 	::=	 	* | +
+;   relop	 	::=	 	< | <= | = >= | > | !=
+;   aloc	 	::=	 	aloc?
+;   label	 	::=	 	label?
 ;   int64	 	::=	 	int64?
 
-; values-lang-v3 -> values-unique-lang-v3
-(define (uniquify vlv3)
-  ; let env be a dict when maps name to aloc
+; values-lang-v5 -> values-unique-lang-v5
+(define (uniquify vlv5)
+  (define proc-env (make-hash))
+
+  ; let env be a dict which maps (procedure-name : label)
+  ;; name -> label
+  (define (proc-name->label proc-name)
+    (dict-ref! proc-env proc-name (fresh-label proc-name)))
+
+  ; let env be a dict which maps name to aloc
   ; name env -> aloc
-  (define (name->aloc x env)
-    (dict-ref env x (fresh x)))
+  (define (name->aloc x arg-env)
+    (dict-ref arg-env x (fresh x)))
+
+  ;; Takes triv in argument position and assigns aloc if triv is a name, otherwise returns
+  ;; triv if triv is int64.
+  ;; (values-lang-v5 triv?) -> (values-lang-v5 triv | int64)
+  (define (triv->arg arg arg-env)
+    (if (int64? arg)
+        arg
+        (name->aloc arg arg-env)))
+
   ; values-lang-v3-triv env -> values-unique-lang-v3-triv
   (define (uniquify-triv triv env)
     (match triv
       [(? int64?) triv]
       [(? name?) (dict-ref env triv (λ () (error (format "undefined: ~a" triv))))]))
-  ; (listof values-lang-v3-name) (listof value-lang-v3-value) env ->
-  ;     (list (listof (list values-unique-lang-v3-name values-unique-lang-v3-value)) env)
+
+  ; (listof values-lang-v5-name) (listof value-lang-v5-value) env ->
+  ;     (list (listof (list values-unique-lang-v5-name values-unique-lang-v5-value)) env)
   (define (uniquify-pairs xs vals env)
-    (if (not (equal? (set-count (list->set xs)) (length xs)))
-        (error (format "duplicate declaration in the same let: ~a" xs))
-        (void))
-    (let ([unialocs (map (λ (x) (name->aloc x env)) xs)]
-          [univals (map (λ (val) (uniquify-value val env)) vals)])
-      (list (map list unialocs univals)
-            (foldl (λ (x aloc env) (dict-set env x aloc)) env xs unialocs))))
+    (when (not (equal? (set-count (list->set xs)) (length xs)))
+      (error (format "duplicate declaration in the same let: ~a" xs)))
+    (define copy-env (set-copy env))
+    (values (map (λ (x val)
+                   (define new-aloc (fresh x)) ;; always a fresh aloc in this context
+                   (dict-set! copy-env x new-aloc)
+                   ;; new binding only applies within directly nested values
+                   (define unique-value (uniquify-value val (dict-set env x new-aloc)))
+                   ;; produce a pair (new-aloc value)
+                   (list new-aloc unique-value))
+                 xs
+                 vals)
+            ;; return modified env to apply to the body of let
+            copy-env))
 
   (define (uniquify-pred pred env)
     (match pred
       [`(,relop ,triv1 ,triv2)
        #:when (memq relop '(< <= = >= > !=))
        `(,relop ,(uniquify-triv triv1 env) ,(uniquify-triv triv2 env))]
-      [`(true) pred]
-      [`(false) pred]
       [`(not ,pred) `(not ,(uniquify-pred pred env))]
-      [`(let ([,xs ,vals] ...) ,pred1)
-       #:when ((listof name?) xs)
-       (match-let ([`(,pairs ,env2) (uniquify-pairs xs vals env)])
-         `(let ,pairs ,(uniquify-pred pred1 env2)))]
+      [`(let ([,xs ,vals] ...) ,pred)
+       ;    #:when ((listof name?) xs) ;; isn't it guaranteed to be (listof name) ?
+       (define-values (bindings new-env) (uniquify-pairs xs vals env))
+       `(let ,bindings ,(uniquify-pred pred new-env))]
       [`(if ,pred1 ,pred2 ,pred3)
        `(if ,(uniquify-pred pred1 env)
             ,(uniquify-pred pred2 env)
-            ,(uniquify-pred pred3 env))]))
+            ,(uniquify-pred pred3 env))]
+      [_ pred]
+      ; [`(true) pred]
+      ;   [`(false) pred]
+      ))
 
-  ; values-lang-v3-value env -> values-unique-lang-v3-value
+  ; values-lang-v5-value env -> values-unique-lang-v5-value
   (define (uniquify-value value env)
     (match value
       [`(let ([,xs ,vals] ...) ,val2)
-       #:when ((listof name?) xs)
-       (match-let ([`(,pairs ,env2) (uniquify-pairs xs vals env)])
-         `(let ,pairs ,(uniquify-value val2 env2)))]
-      [`(,binop ,triv1 ,triv2)
-       #:when (and (binop? binop) (triv? triv1) (triv? triv2))
-       `(,binop ,(uniquify-triv triv1 env) ,(uniquify-triv triv2 env))]
+       (define-values (bindings new-env) (uniquify-pairs xs vals env))
+       `(let ,bindings ,(uniquify-value value new-env))]
       [`(if ,pred1 ,pred2 ,pred3)
        `(if ,(uniquify-pred pred1 env)
             ,(uniquify-value pred2 env)
             ,(uniquify-value pred3 env))]
+      ;; reordered to disambiguate with (if) instead
+      ;    #:when (and (binop? binop) (triv? triv1) (triv? triv2))
+      [`(,binop ,triv1 ,triv2) `(,binop ,(uniquify-triv triv1 env) ,(uniquify-triv triv2 env))]
       [(? triv?) (uniquify-triv value env)]))
-  ; tail = value, no need for a separate function
-  ; values-lang-v3-p env -> values-unique-lang-v3-p
-  (define (uniquify-p p env)
+
+  (define (uniquify-tail tail arg-env)
+    (match tail
+      [`(let ([,xs ,vals] ...) ,tail)
+       ;#:when ((listof name?) xs) ;; is this necessary?
+       (define-values (bindings new-env) (uniquify-pairs xs vals arg-env))
+       `(let ,bindings ,(uniquify-tail tail new-env))]
+      [`(if ,pred ,t1 ,t2)
+       `(if ,(uniquify-pred pred arg-env)
+            ,(uniquify-tail t1 arg-env)
+            ,(uniquify-tail t2 arg-env))]
+      ;; call to function x, with trivs as args
+      [`(call ,x ,trivs ...)
+       `(call ,(proc-name->label x) ,@(map (λ (arg) (triv->arg arg arg-env)) trivs))]
+      [_ (uniquify-value tail arg-env)]))
+
+  (define (uniquify-definitions def arg-env)
+    (match def
+      [`(define ,proc-name (lambda (,args ...) ,tail))
+       `(define ,(proc-name->label proc-name)
+          (lambda (,@(map (λ (arg) (dict-set arg-env arg (fresh arg))) args))
+            ,(uniquify-tail tail arg-env)))]))
+
+  ; (values-lang-v5-p env) -> (values-unique-lang-v5 p)
+  (define (uniquify-p p)
     (match p
-      [`(module ,tail) `(module ,(uniquify-value tail env))]))
-  (uniquify-p vlv3 '()))
+      [`(module ,defs ...
+          ,tail)
+       `(module ,@(map (λ (def) (uniquify-definitions def (make-hash))) defs)
+                (uniquify-tail tail (make-hash))
+          )]))
+
+  (uniquify-p vlv5))
 
 (module+ test
   (require rackunit)

57626c127f96bbf5a41a68aa6baa325762e3c32d
tkozek <tkozek@students.cs.ubc.ca>
2026-02-28 14:09:39 -0800

    select-instructions: Updated documentation, extended to support imp-cmf-lang-v5, reduced redundancy.

--- select-instructions.rkt
+++ select-instructions.rkt
@@ -25,42 +25,42 @@
 
 ; int64	 	::=	 	int64?
 
-;imp-cmf-lang-v4  -> Asm-pred-lang-v4
+;; (imp-cmf-lang-v5 p) -> (asm-pred-lang-v5 p)
+;; Compiles imp-cmf-lang-v5 to asm-pred-lang-v5 by selecting appropriate sequences of abstract
+;; assembly instructions to implement the operations of the source language.
 (define (select-instructions p)
-  ; (Imp-cmf-lang-v4 value) -> (List-of (Asm-pred-lang-v4 effect)) and (Asm-pred-lang-v4 aloc)
+
+  ; (imp-cmf-lang-v5 value) -> (List-of (asm-pred-lang-v5 effect)) and (asm-pred-lang-v5 aloc)
   ; Assigns the value v to a fresh temporary, returning two values: the list of
-  ; statements the implement the assignment in Loc-lang, and the aloc that the
+  ; statements that implement the assignment in asm-pred-lang, and the aloc that the
   ; value is stored in.
   (define (assign-tmp v)
     (define tmp (fresh 'tmp))
     (match v
       [`(,binop ,triv1 ,triv2)
        (list (list `(set! ,tmp ,triv1) `(set! ,tmp (,binop ,tmp ,triv2))) tmp)]
-      [triv (list (list `(set! ,tmp ,triv)) tmp)]))
+      [_ (list (list `(set! ,tmp ,v)) tmp)]))
 
-  (define (select-pred pred [begun #f])
+  (define (select-pred pred)
     (match pred
-      [`(,relop ,triv1 ,triv2)
-       #:when (memq relop '(< <= = >= > !=))
-       (if (not (aloc? triv1))
-           (match-let ([`(,fxs ,aloc) (assign-tmp triv1)])
-             `(begin
-                ,@fxs
-                (,relop ,aloc ,triv2)))
-           pred)]
-      [`(true) pred]
-      [`(false) pred]
       [`(not ,pred) `(not ,(select-pred pred))]
       [`(begin
           ,fxs ...
           ,pred)
-       (append `(begin) (foldr append '() (map select-effect fxs)) (list (select-pred pred #t)))]
+       (append `(begin) (foldr append '() (map select-effect fxs)) (list (select-pred pred)))]
       [`(if ,pred1 ,pred2 ,pred3)
        `(if ,(select-pred pred1)
             ,(select-pred pred2)
-            ,(select-pred pred3))]))
+            ,(select-pred pred3))]
+      [`(,relop ,triv1 ,triv2)
+       #:when (int64? triv1)
+       (match-let ([`(,fxs ,aloc) (assign-tmp triv1)])
+         `(begin
+            ,@fxs
+            (,relop ,aloc ,triv2)))]
+      [_ pred]))
 
-  ; (imp-cmf-lang-v4 tail) [bool] -> (Asm-pred-lang-v4 tail)
+  ; (imp-cmf-lang-v5 tail) [bool] -> (asm-pred-lang-v5 tail)
   (define (select-tail e [begun #f])
     (match e
       [`(begin
@@ -78,6 +78,7 @@
          (if (not begun)
              result
              `(,result)))]
+      [`(jump ,trg ,loc) e]
       [_
        (match-let ([`(,fxs ,atail) (select-value e)])
          (append (if (not begun)
@@ -85,43 +86,50 @@
                      '())
                  fxs
                  (list atail)))]))
-  ; (Imp-cmf-lang-v4 value) -> (list (listof (Asm-pred-lang-v4 effect)) (Asm-pred-lang-v4 tail))
+
+  ; (imp-cmf-lang-v5 value) -> (list (listof (asm-pred-lang-v5 effect)) (asm-pred-lang-v5 tail))
   (define (select-value e)
     (match e
       [`(,binop ,triv1 ,triv2)
        (match-let ([`(,fxs ,aloc) (assign-tmp e)])
          (list fxs `(halt ,aloc)))]
-      [triv `(() (halt ,triv))]))
+      [_ `(() (halt ,e))]))
 
-  ; (Imp-cmf-lang-v4 value) -> (listof (Asm-pred-lang-v4 effect))
-  (define (value->effect* e prev-aloc)
-    (match e
-      [`(,binop ,triv1 ,triv2)
-       `((set! ,prev-aloc ,triv1) (set! ,prev-aloc (,binop ,prev-aloc ,triv2)))]
-      [_ `((set! ,prev-aloc ,e))]))
+  ; (imp-cmf-lang-v5 value) -> (listof (asm-pred-lang-v5 effect))
+  ;; selects appropriate abstract assembly instructions for values in effect position
+  (define (value->effect* loc value)
+    (match value
+      [`(,binop ,opand1 ,opand2)
+       #:when (not (eq? opand1 loc))
+       `((set! ,loc ,opand1) (set! ,loc (,binop ,loc ,opand2)))]
+      [_ `((set! ,loc ,value))]))
 
-  ; (Imp-cmf-lang-v4 effect) -> (listof (Asm-pred-lang-v4 effect))
+  ; (imp-cmf-lang-v5 effect) -> (listof (asm-pred-lang-v5 effect))
   (define (select-effect e)
     (match e
-      ;special case
-      [`(set! ,aloc (,binop ,aloc ,triv)) `(,e)]
-      [`(set! ,aloc ,value) (value->effect* value aloc)]
+      [`(set! ,loc ,rest) (value->effect* loc rest)]
       [`(begin
           ,fxs ...
           ,fx)
-       (list (append `(begin) (foldr append '() (map select-effect fxs)) (select-effect fx)))
-       ;  (append (foldr append '() (map select-effect fxs))
-       ;          (select-effect fx))
-       ]
+       (list (append `(begin) (foldr append '() (map select-effect fxs)) (select-effect fx)))]
       [`(if ,pred ,effect1 ,effect2)
        `((if ,(select-pred pred)
              ,(select-effect effect1)
              ,(select-effect effect2)))]))
 
-  (match p
-    [`(module ,tail)
-     `(module () ,(select-tail tail)
-        )]))
+  ;; (imp-cmf-lang-v5 definition) -> (imp-cmf-lang-v5 definition)
+  (define (select-def def)
+    (match def
+      [`(define ,label ,tail) `(define ,label ,(select-tail tail))]))
+
+  (define (select-p p)
+    (match p
+      [`(module ,defs ...
+          ,tail)
+       `(module () ,@(map select-def defs)
+          ,(select-tail tail))]))
+
+  (select-p p))
 
 (module+ test
   (require rackunit)

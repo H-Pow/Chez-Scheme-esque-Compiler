36f47f6c7a4d1117b551297d75c0d3ce566089fc
tkozek <tkozek@students.cs.ubc.ca>
2026-04-05 20:06:32 -0700

    Initial commit for convert-closures

---
+++ convert-closures.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(require cpsc411/compiler-lib
+         cpsc411/langs/v9
+         "common.rkt")
+
+(provide convert-closures)
+
+
+;; (lam-free-lang-v9 p) → (closure-lang-v9 p)
+;; Performs closure conversion, converting all procedures into explicit closures.
+(define (convert-closures p)
+    
+
+
+(define (convert-p p)
+    )
+(convert-p p)
+)
+
+
